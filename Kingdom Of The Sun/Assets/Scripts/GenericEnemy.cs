4c3026bce2ece2c391be35ff71a2aefdfab6a8bf
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-06 19:12:38 +0200

    unnecessary change?

---
+++ Kingdom Of The Sun/Assets/Scripts/GenericEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericEnemy : MonoBehaviour
+{
+    //base stats Enemy
+    protected int health;
+    protected int damage;
+    protected int walkingSpeed;
+    protected int attackSpeed;
+    protected STATES state;
+
+    //states of the Enemy
+    protected enum STATES
+    {
+        WONDERING,
+        CHASE,
+        ATTACK
+    }
+
+    //target of this Enemy and target it will attempt to attack
+    protected GameObject target;
+    protected Vector3 ownPosition;
+    
+    //distance between player and this Enemy
+    protected float distance;
+
+    public void setHealth(int tempHealth)
+    {
+        health = tempHealth;
+    }
+
+    public void setDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ Kingdom Of The Sun/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : GenericEnemy
+{
+    void Start()
+    {
+        state = STATES.WONDERING;
+        health = 100;
+        walkingSpeed = 10;
+        attackSpeed = 10;
+    }
+    
+    void Update()
+    {
+        
+    }
+}
