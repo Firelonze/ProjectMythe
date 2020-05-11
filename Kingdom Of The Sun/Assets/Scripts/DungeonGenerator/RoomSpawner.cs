cf96f96875ab145c29b35cbf1cf2f7c66bbb74e3
Robin van den Bor <Firelonze2002@outlook.com>
2020-05-11 11:49:11 +0200

    DungeonGenerator&SpawnerMade

---
+++ Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : RoomTemplates
+{
+    [SerializeField]private int openingDirection = 1;
+    // 1 is top
+    // 2 is right
+    // 3 is bottom
+    // 4 is left
+
+    private int rand;
+    private bool spawned = false;
+    private float waitTime = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, waitTime);
+        Invoke("Spawn", 0.1f);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            switch (openingDirection)
+            {
+                case 1:
+                    rand = Random.Range(0, topRooms.Length);
+                    Instantiate(topRooms[rand], transform.position, transform.rotation);
+                break;
+                case 2:
+                    rand = Random.Range(0, rightRooms.Length);
+                    Instantiate(rightRooms[rand], transform.position, transform.rotation);
+                    break;
+                case 3:
+                    rand = Random.Range(0, bottomRooms.Length);
+                    Instantiate(bottomRooms[rand], transform.position, transform.rotation);
+                    break;
+                case 4:
+                    rand = Random.Range(0, leftRooms.Length);
+                    Instantiate(leftRooms[rand], transform.position, transform.rotation);
+                    break;
+            }
+            spawned = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                //blocks off opening
+                Instantiate(closedRooms, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+}
---
+++ Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    [SerializeField] protected GameObject[] topRooms;
+    [SerializeField] protected GameObject[] bottomRooms;
+    [SerializeField] protected GameObject[] rightRooms;
+    [SerializeField] protected GameObject[] leftRooms;
+
+    [SerializeField] protected GameObject closedRooms;
+
+    //Dictionary<int, List<GameObject>> Rooms = new Dictionary<int, List<GameObject>>();
+    private void Start()
+    {
+        //Rooms[1] = new List<GameObject>();
+        //Debug.Log(Rooms[1]);
+        //inventory[Keys.MACHINE_GUN] = new List<GameObject>();
+        //Rooms[1].Add(topRooms[0]);
+
+    }
+
+}
