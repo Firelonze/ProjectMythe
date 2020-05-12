3a704d2816a011695f0fc44778df8359325c86f5
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-12 17:09:31 +0200

    handlers done

---
+++ Kingdom Of The Sun/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    public void setAnimation(int i)
+    {
+        animator.SetInteger("IntName", i /*animation state*/);
+        if (i == 0 /*Death Anim State*/)
+        {
+            StartCoroutine(Death());
+        }
+    }
+
+    private IEnumerator Death()
+    {
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        yield return new WaitForSeconds(clips[0 /* death anim clip number */].length);
+        Destroy(gameObject);
+    }
+}
---
+++ Kingdom Of The Sun/Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandler : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private AudioClip[] audioClips;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playAudio(int i)
+    {
+        audioSource.clip = audioClips[i];
+        audioSource.Play();
+    }
+
+    public void stopAudio()
+    {
+        audioSource.Stop();
+    }
+}
