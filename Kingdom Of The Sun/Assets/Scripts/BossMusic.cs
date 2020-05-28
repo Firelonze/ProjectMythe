4e218ee8d4754b35f3eea5656c93191e3ed4f743
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-28 14:09:13 +0200

    whatever

--- Kingdom Of The Sun/Assets/Scripts/BossMusic.cs
+++ Kingdom Of The Sun/Assets/Scripts/BossMusic.cs
@@ -8,15 +8,40 @@ public class BossMusic : MonoBehaviour
 
     [SerializeField] private AudioClip[] audioClips;
 
-    private int currentClip;
+    private int currentClip = 0;
+
+    private float timer;
 
     private void Awake()
     {
-        audioSource.GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
-    
-    private void PlayAudio(AudioClip[currentClip])
-    {
 
+    private void Start()
+    {
+        PlayAudio();
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            PlayAudio();
+        }
+    }
+
+    public void SetMusic(int tempInt)
+    {
+        currentClip = tempInt;
+    }
+
+    private void PlayAudio()
+    {
+        audioSource.clip = audioClips[currentClip];
+        audioSource.Play();
+        timer = audioSource.clip.length;
+        if(currentClip == 0)
+        {
+            currentClip++;
+        }
     }
 }
