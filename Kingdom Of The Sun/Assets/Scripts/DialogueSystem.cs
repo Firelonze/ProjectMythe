5fb3fe9ddf89479b12cb3d3f6c6edc7c60c830eb
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-05-29 02:27:56 +0200

    Weetje, slapen klinkt goed rond nu

---
+++ Kingdom Of The Sun/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    private string[] textPlayer;
+    private string[] textTezca;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+        textPlayer[0] = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
