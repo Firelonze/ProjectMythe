1364cc76ed686481c422f0627c873b81e765c8ce
Robin van den Bor <Firelonze2002@outlook.com>
2020-06-12 04:34:19 +0200

    RandomTileSpawner

--- Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomSpawner.cs
+++ Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomSpawner.cs
@@ -2,10 +2,151 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UIElements;
 
-public class RoomSpawner : OpeningRaycast
+public class RoomSpawner : RoomTemplates
 {
- 
+    [SerializeField]private GameObject[] sectionsPositions;
+    private int currentSection = 1;
+    private int chosenSection;
+    private int exitSection;
+    private void Start()
+    {
+        exitSection = Random.Range(0, 8);
+        SectionSelector();
+    }
+    private void TileSpawner()
+    {
+        switch (currentSection)
+        {
+            case 1:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section1[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else 
+                {
+                    Instantiate(section1Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 2:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section2[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section2Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 3:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section3[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section3Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 4:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section4[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section4Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 5:
+                Instantiate(Entrance[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                currentSection += 1;
+                SectionSelector();
+                break;
+            case 6:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section6[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section6Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 7:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section7[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section7Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 8:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section8[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+                else
+                {
+                    Instantiate(section8Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    currentSection += 1;
+                    SectionSelector();
+                    break;
+                }
+            case 9:
+                if (currentSection != exitSection)
+                {
+                    Instantiate(section9[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    break;
+                }
+                else
+                {
+                    Instantiate(section9Exit[chosenSection], sectionsPositions[chosenSection].transform.position, Quaternion.identity);
+                    break;
+                }
+        }
 
+    }
+    private void SectionSelector()
+    {
+        chosenSection = Random.Range(0,1);
+        TileSpawner();
+    }
 }
--- Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
+++ Kingdom Of The Sun/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class RoomTemplates : MonoBehaviour
 {
-    [SerializeField] protected GameObject[] topRow;
-    [SerializeField] protected GameObject[] bottomRow;
-    [SerializeField] protected GameObject[] leftRow;
-    [SerializeField] protected GameObject[] rightRow;
+    [SerializeField] protected GameObject[] section1;
+    [SerializeField] protected GameObject[] section2;
+    [SerializeField] protected GameObject[] section3;
+    [SerializeField] protected GameObject[] section4;
+    [SerializeField] protected GameObject[] section5;
+    [SerializeField] protected GameObject[] section6;
+    [SerializeField] protected GameObject[] section7;
+    [SerializeField] protected GameObject[] section8;
+    [SerializeField] protected GameObject[] section9;
 
-    [SerializeField] protected GameObject[] topLeftCorner;
-    [SerializeField] protected GameObject[] topRightCorner;
-    [SerializeField] protected GameObject[] bottomLeftCorner;
-    [SerializeField] protected GameObject[] bottomRightCorner;
+    [SerializeField] protected GameObject[] section1Exit;
+    [SerializeField] protected GameObject[] section2Exit;
+    [SerializeField] protected GameObject[] section3Exit;
+    [SerializeField] protected GameObject[] section4Exit;
+    [SerializeField] protected GameObject[] section6Exit;
+    [SerializeField] protected GameObject[] section7Exit;
+    [SerializeField] protected GameObject[] section8Exit;
+    [SerializeField] protected GameObject[] section9Exit;
 
-    [SerializeField] protected GameObject[] center;
-
+    [SerializeField] protected GameObject[] Entrance;
 }
