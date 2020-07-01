d487cea1abd9b1f4e9de5cd75b6308dd21db3a5d
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-07-01 07:49:41 +0200

    Boss room finished 3

---
+++ Kingdom Of The Sun/Assets/BossHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BossHealth : MonoBehaviour
+{
+    public int health;
+
+
+
+    private void Update()
+    {
+        if(health == 0)
+        {
+
+            GameObject.Find("Camera").gameObject.transform.parent = null;
+            GameObject.Find("Player").SetActive(false);
+            GameObject.Find("CrossHair").SetActive(false);
+            GameObject.Find("Subs").SetActive(false);
+            GameObject.Find("Image").SetActive(false);
+
+            GameObject.Find("EndingCinematic").GetComponent<VideoPlayer>().Play();
+            //do cutsccene
+        }
+    }
+}
