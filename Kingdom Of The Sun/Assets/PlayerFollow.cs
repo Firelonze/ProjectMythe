2122e422f3166f88ea9463ff42f9d20ebe7250bf
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-07-01 07:51:05 +0200

    Boss room finished

---
+++ Kingdom Of The Sun/Assets/PlayerFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 yeet = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+        //player.transform
+        transform.LookAt(yeet);
+    }
+
+    public float getRotation()
+    {
+        return transform.rotation.eulerAngles.y;
+    }
+}
