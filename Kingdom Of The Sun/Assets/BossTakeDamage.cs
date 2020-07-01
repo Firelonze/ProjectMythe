beee7c0a67322f5d58957c6b95b9190d31620142
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2020-07-01 07:49:56 +0200

    Boss room finished 4

---
+++ Kingdom Of The Sun/Assets/BossTakeDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTakeDamage : MonoBehaviour
+{
+    AnimationHandler animHandler;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 14)
+        {
+            GameObject.Find("Boss").GetComponent<BossHealth>().health -= 1;
+
+            animHandler.setAnimation(1/*damage anim*/);
+        }
+    }
+}
